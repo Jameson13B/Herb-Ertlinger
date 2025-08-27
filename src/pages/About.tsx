64c656474e1265e7f1ef2d5f91bad53b0af475ff
christopher-brown1 <crouton_limpid.0g@icloud.com>
2025-08-27 16:08:35 -0600

    About page content

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,3 +1,79 @@
+import CactusCrowIcon from "../assets/cactusCrowIcon.svg";
+import BioImage from "../assets/BioImage.jpg";
+import InstaLarge from "../assets/instaLarge.svg";
+import FacebookLarge from "../assets/facebookLarge.svg";
+import EmailLarge from "../assets/emailLarge.svg";
+
+import styles from "./about.module.css";
+
 export const About = () => {
-  return <div>About</div>
-}
+  return (
+    <>
+      {/* TODO: Banner */}
+      <div className={styles.headerContainer}>
+        <img src={CactusCrowIcon} className={styles.headerImg} />
+        <h1 className={styles.headerTxt}>About</h1>
+      </div>
+      <div className={styles.aboutContent}>
+        <p>
+          Virginia Gontika is a landscape photographer whose work is deeply
+          rooted in a lifelong love for wild, ancient landscapes. Originally
+          from Greece, she felt a powerful connection to the American Southwest,
+          particularly the Colorado Plateau, and eventually made Utah her home.
+          Now based in Salt Lake City with her beautiful family, Virginia draws
+          daily inspiration from the rugged desert terrain that surrounds her.
+        </p>
+        <p>
+          After picking up a camera in 2018, she immediately fell in love with
+          photography. What began as a creative outlet soon evolved into a
+          deeper calling: to document and protect the desert landscapes that had
+          captured her heart. In 2024, she founded Golden Crown Photography and
+          began pursuing landscape photography professionally.
+        </p>
+        <div className={styles.splitContent}>
+          <img src={BioImage} className={styles.bioImage} />
+          <div className={styles.splitTxt}>
+            <p>
+              Her work is more than aesthetic, it is a form of advocacy. Through
+              her images, Virginia seeks to educate others on the fragility,
+              dangers, and profound beauty of the Colorado Plateau. Each
+              photograph tells a layered story of geological wonder, ecological
+              sensitivity, and cultural richness. Her mission is to raise
+              awareness of the environmental threats facing the region and to
+              inspire deeper care for these irreplaceable lands.
+            </p>
+            <p>
+              Virginia’s photography is grounded in reverence. She sees the
+              camera as a bridge connecting viewers to the land and encouraging
+              a sense of awe, respect, and responsibility. Her compositions
+              highlight powerful skies, subtle textures, and sacred spaces that
+              invite both wonder and reflection.
+            </p>
+            <p>
+              Virginia shares the beauty and urgency of the Colorado Plateau
+              with a growing audience, one print, one story, and one landscape
+              at a time.
+            </p>
+            {/* TODO: Check button styling */}
+            <button></button>
+          </div>
+        </div>
+        <div className={styles.socialContainer}>
+          <div className={styles.socialTab}>
+            <img src={InstaLarge} />
+            <p>Instagram</p>
+          </div>
+          <div className={styles.socialTab}>
+            <img src={FacebookLarge} />
+            <p>Facebook</p>
+          </div>
+          <div className={styles.socialTab}>
+            <img src={EmailLarge} />
+            <p>Email</p>
+          </div>
+        </div>
+      </div>
+      {/* TODO: Banner */}
+    </>
+  );
+};
