0719b624c9fca76dc5e162cea89057b0d5bfcd2d
Jameson Brown <jamesonbrown@neolivity.com>
2025-09-21 23:55:43 -0600

    Update state management to show gallery by default and hide admin; remove SectionHeader from exports

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -2,7 +2,6 @@ import { Button } from "./button/Button"
 import { CustomUpload } from "./customUpload/CustomUpload"
 import { Dropdown } from "./dropdown/Dropdown"
 import { GalleryPost } from "./galleryPost/GalleryPost"
-import { SectionHeader } from "./sectionHeader/SectionHeader"
 import { TagBar } from "./tagBar/TagBar"
 
-export { Button, CustomUpload, Dropdown, GalleryPost, SectionHeader, TagBar }
+export { Button, CustomUpload, Dropdown, GalleryPost, TagBar }
