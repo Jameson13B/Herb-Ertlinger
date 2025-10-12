2c5edae4260a856110948c08e083c05f5def67ea
Christopher-Brown1 <crouton_limpid.0g@icloud.com>
2025-10-12 17:43:51 -0600

    CartItem component created

---
+++ src/components/cartItem/CartItem.tsx
@@ -0,0 +1,41 @@
+import styles from "./CartItem.module.css"
+import { Button } from "../button/Button"
+import aboutBanner from "../../assets/aboutBanner.png"
+import trash from "../../assets/trash.svg"
+
+export const CartItem = () => {
+  return (
+    <div className={styles.cartItem}>
+      <div className={styles.namePicture}>
+        <h2 className={styles.itemName}>Item.name</h2>
+        <img
+          src={aboutBanner}
+          alt='About Banner'
+          style={{
+            height: "160px",
+            alignSelf: "stretch",
+            aspectRatio: "3/2",
+          }}
+        />
+      </div>
+      <div className={styles.variants}>
+        <div className={styles.variant}>
+          <p>Size</p>
+          <div className={styles.variantValue}>24x36</div>
+        </div>
+        <div className={styles.variant}>
+          <p>Material</p>
+          <div className={styles.variantValue}>Canvas</div>
+        </div>
+        <div className={styles.variant}>
+          <p>Price</p>
+          <div className={styles.variantValue}>$100.00</div>
+        </div>
+        <div className={styles.buttonContainer}>
+          <Button variant='secondary'>Edit</Button>
+          <img src={trash} alt='Trash Icon' />
+        </div>
+      </div>
+    </div>
+  )
+}
